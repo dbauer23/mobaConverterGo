0ff73a6aa367db93417047e6569884f091c5b6f7
DominikBauer1 <dominik@dpbauer.de>
2024-07-19 23:50:47 +0200

    added go-bindata to generate and bindata to gitignore

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
+// Install go-bindata if not present. This is a convenience directive for development environments.
+//go:generate go install github.com/go-bindata/go-bindata/...
 //go:generate go-bindata -o .\internal\config\config.json.go -pkg config .\data\config.json
 
 import (
